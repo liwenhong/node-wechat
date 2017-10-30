d3bd1490fa5d9e9490d1229914d49938a70ba510
李文洪 <784327282@qq.com>
2017-10-30 11:00:26 +0800

    post参数获取

--- app.js
+++ app.js
@@ -1,47 +1,51 @@
 const express = require('express'), //引入express框架
     crypto = require('crypto'), //引入加密模块
-    config = require('./config');//引入配置文件
-    wechat = require('./wechat/wechat')
+    config = require('./config'),//引入配置文件
+    wechat = require('./wechat/wechat'),
+    bodyParser = require('body-parser');
 
 //实例化express
 var app = new express();
 var wechatApp = new wechat(config); //实例wechat对象
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 //设置跨域访问
-app.all('*', function(req, res, next) {
+app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://192.168.1.200:8080");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Credentials", 'true');
-    res.header("X-Powered-By",' 3.2.1');
-    res.header("Content-Type", "application/x-www-form-urlencoded");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Content-Type", "application/json");
     next();
- });
+});
 app.get('/', function (req, res) {
-    wechatApp.auth(req,res);
+    wechatApp.auth(req, res);
 })
 app.get('/test', function (req, res) {
-   res.send('这个是test方法')
+    console.log(req.query.name)
+    console.log(req.query)
+    res.send('这个是test方法')
 })
-app.get('/getAccessToken',function(req,res){
-    wechatApp.getAccessToken().then(function(data){
+app.get('/getAccessToken', function (req, res) {
+    wechatApp.getAccessToken().then(function (data) {
         res.send(data);
     })
 })
-app.get('/createMenus',function(req,res){
-    wechatApp.creatMeanus().then(function(data){
+app.get('/createMenus', function (req, res) {
+    wechatApp.creatMeanus().then(function (data) {
         res.send(data);
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.log(err);
     });
 })
 //用于处理所有进入 8090 端口 post 的连接请求
-app.post('/',function(req,res){
-  wechatApp.handleMsg(req,res);
+app.post('/', function (req, res) {
+    wechatApp.handleMsg(req, res);
 });
 
-app.post('/testP',function(req,res){
-    console.log(req.query);
-    
+app.post('/testP', function (req, res) {
+    console.log(req.body);
+    res.send('hello this is post methods!;')
 })
 app.listen(8090);
